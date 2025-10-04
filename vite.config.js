c59eb1a6f94da7634e0ff0f75a12afcb7453d37d
mlawsonfpms <mlawson@ecsdfl.us>
2025-10-04 15:00:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Replace YOUR_REPO_NAME below with your actual repo name (for example: 'roller-coaster-sim')
 export default defineConfig({
   plugins: [react()],
-  base: "/roller-coaster-sim/", // 👈 must match your GitHub repo name
+  base: '/roller-coaster-sim/', 
 });
